e170d9c8fff186b8fb0eb2c040ad44cb1a2da74c
jwilliams436 <jake.williams@gatech.edu>
2019-08-14 12:25:17 -0400

    Dummy Data with Schema for GraphQL

--- be/gql.go
+++ be/gql.go
@@ -1,106 +1,91 @@
 package main
 
 import (
-	"context"
-	"net/http"
+    "context"
+    "net/http"
 
-	"github.com/samsarahq/thunder/graphql"
-	"github.com/samsarahq/thunder/graphql/graphiql"
-	"github.com/samsarahq/thunder/graphql/introspection"
-	"github.com/samsarahq/thunder/graphql/schemabuilder"
+    "github.com/samsarahq/thunder/graphql"
+    "github.com/samsarahq/thunder/graphql/graphiql"
+    "github.com/samsarahq/thunder/graphql/introspection"
+    "github.com/samsarahq/thunder/graphql/schemabuilder"
 )
 
 type Server struct {
 }
 
-type RoleType int32
-type User struct {
-	Id        int
-	FirstName string
-	LastName  string
-	Role      RoleType
-}
-
-func (s *Server) registerUser(schema *schemabuilder.Schema) {
-	object := schema.Object("User", User{})
-	object.Key("id")
-
-  object.FieldFunc("firstName", func(u *User) string {
-    return u.FirstName + "A"
-  })
-
-	object.FieldFunc("fullName", func(u *User) string {
-		return u.FirstName + " " + u.LastName
-	})
-}
-
 type Args struct {
-	Role *RoleType
 }
 
-func userListRet(ctx context.Context, args Args) ([]*User, error) {
-		return []*User{
-			{
-				Id:        1,
-				FirstName: "Bob",
-				LastName:  "Johnson",
-				Role:      RoleType(1),
-			},
-			{
-				Id:        2,
-				FirstName: "Chloe",
-				LastName:  "Kim",
-				Role:      RoleType(1),
-			},
-		}, nil
-	}
+func landPageListRet(ctx context.Context, args Args) ([]*LandingPage, error) {
+    mc := MCQuestion{
+        Question: "How are you",
+        Answers:  []string{"good"},
+    }
+
+    oe := OpenEndedQuestion{
+        Question: "Why",
+    }
+
+    return []*LandingPage{
+        {
+            Title:    "HummingBird",
+            SubTitle: "Sing like your favorite artists",
+            JoinEmailList: JoinEmailList{
+                JoinPrompt:     "Keep up to date",
+                JoinButtonText: "Join",
+            },
+            Questions: Questions{
+                QuestionsPrompt: "Help us build something for you",
+                McQuestions:     []MCQuestion{mc},
+            },
+        },
+        {
+            Title:    "Durgs",
+            SubTitle: "Do some durgs",
+            JoinEmailList: JoinEmailList{
+                JoinPrompt:     "Find out about durgs",
+                JoinButtonText: "Segrada Familia",
+            },
+            Questions: Questions{
+                QuestionsPrompt:    "Help us build something for you",
+                OpenEndedQuestions: []OpenEndedQuestion{oe},
+            },
+        },
+    }, nil
+}
 
 func (s *Server) registerQuery(schema *schemabuilder.Schema) {
-	object := schema.Query()
+    object := schema.Query()
 
-	var tmp RoleType
-	schema.Enum(tmp, map[string]RoleType{
-		"user":          RoleType(1),
-		"manager":       RoleType(2),
-		"administrator": RoleType(3),
-	})
-
-	object.FieldFunc("users", userListRet)
+    object.FieldFunc("landingPages", landPageListRet)
 }
 
 func (s *Server) registerMutation(schema *schemabuilder.Schema) {
-	object := schema.Mutation()
+    object := schema.Mutation()
 
-	object.FieldFunc("echo", func(ctx context.Context, args struct{ Text string }) (string, error) {
-		return args.Text, nil
-	})
-
-	object.FieldFunc("echoEnum", func(ctx context.Context, args struct {
-		EnumField RoleType
-	}) (RoleType, error) {
-		return args.EnumField, nil
-	})
+    object.FieldFunc("echo", func(ctx context.Context, args struct{ Text string }) (string, error) {
+            return args.Text, nil
+    })
 }
 
 func (s *Server) Schema() *graphql.Schema {
-	schema := schemabuilder.NewSchema()
+    schema := schemabuilder.NewSchema()
 
-	s.registerUser(schema)
-	s.registerQuery(schema)
-	s.registerMutation(schema)
+    s.registerQuery(schema)
+    s.registerMutation(schema)
 
-	return schema.MustBuild()
+    return schema.MustBuild()
 }
 
 func main() {
-	server := &Server{}
-	graphqlSchema := server.Schema()
-	introspection.AddIntrospectionToSchema(graphqlSchema)
+    server := &Server{}
+    graphqlSchema := server.Schema()
+    introspection.AddIntrospectionToSchema(graphqlSchema)
 
-	http.Handle("/graphql", graphql.Handler(graphqlSchema))
-	http.Handle("/graphiql/", http.StripPrefix("/graphiql/", graphiql.Handler()))
+    http.Handle("/graphql", graphql.Handler(graphqlSchema))
+    http.Handle("/graphiql/", http.StripPrefix("/graphiql/", graphiql.Handler()))
 
-	if err := http.ListenAndServe(":3030", nil); err != nil {
-		panic(err)
-	}
+    if err := http.ListenAndServe(":3030", nil); err != nil {
+        panic(err)
+    }
 }
