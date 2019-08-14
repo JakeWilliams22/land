12a0e9c063f6f860b182b69b032548ad63aca0bd
JakeWilliams22 <jakemwilliams2@gmail.com>
2019-08-14 12:30:43 -0400

    Dummy Data with Schema for GraphQL (#9)

---
+++ be/structs.go
@@ -0,0 +1,30 @@
+package main
+
+type LandingPage struct {
+        Title         string
+        SubTitle      string
+        JoinEmailList JoinEmailList
+        Questions     Questions
+}
+
+type Questions struct {
+        QuestionsPrompt string
+        //TODO Figure out how to make this an interface. Thunder seems to not support it.
+        //TODO Figure out question ordering if ^^ can't work
+        McQuestions        []MCQuestion
+        OpenEndedQuestions []OpenEndedQuestion
+}
+
+type MCQuestion struct {
+        Question string
+        Answers  []string
+}
+
+type OpenEndedQuestion struct {
+        Question string
+}
+
+type JoinEmailList struct {
+        JoinPrompt     string
+        JoinButtonText string
+}
