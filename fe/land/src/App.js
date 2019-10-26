3badc80f79fb601e8f9e17c8288dfb87eee29712
Jake Williams <jakemwilliams2@gmail.com>
2019-10-26 12:54:27 -0400

    Graphql front end initial commit

--- fe/land/src/App.js
+++ fe/land/src/App.js
@@ -3,25 +3,47 @@ import './App.css';
 import logo from './assets/logo.svg';
 import JoinEmailList from './components/JoinEmailList/JoinEmailList';
 import QuestionCard from './components/Questions/QuestionCard';
-
+import {ApolloConsumer} from "./index";
+import gql from 'graphql-tag'
 import AdminApp from './admin/AdminApp.js'
 
-function App() {
-  return (
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "title": "",
+        };
+    }
+
+  fetchData = (client) => {
+        client.query({query:
+            gql`
+                {
+                  landingPages {
+                    title
+                  }
+                }`}
+        ).then(response => console.log(response));
+  };
+
+  render = () => (
     <div className="App">
-        <AdminApp />
-        {/*<header className="App-header">*/}
-        {/*    <img src={logo} alt="logo" width="200px"/>*/}
-        {/*    <h1>Hummingbird</h1>*/}
-        {/*    <h3>Sing like your favorite artists</h3>*/}
-        {/*</header>*/}
-        {/*<div className="App-content">*/}
-        {/*    <p>You found us while Hummingbird is still in the Lab!</p>*/}
-        {/*    <p>We'll let you know as soon as it's ready!</p>*/}
-        {/*    <JoinEmailList />*/}
-        {/*    <p>Help us build something better</p>*/}
-        {/*    <QuestionCard/>*/}
-        {/*</div>*/}
+        <ApolloConsumer>
+            {apolloClient => this.fetchData(apolloClient)}
+        </ApolloConsumer>
+        {/*<AdminApp />*/}
+        <header className="App-header">
+            <img src={logo} alt="logo" width="200px"/>
+            <h1>{this.state.title}</h1>
+            <h3>Sing like your favorite artists</h3>
+        </header>
+        <div className="App-content">
+            <p>You found us while Hummingbird is still in the Lab!</p>
+            <p>We'll let you know as soon as it's ready!</p>
+            <JoinEmailList />
+            <p>Help us build something better</p>
+            <QuestionCard/>
+        </div>
     </div>
   );
 }
--- fe/land/src/index.js
+++ fe/land/src/index.js
@@ -4,9 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:3030/graphql/'
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
+const Apollo = React.createContext(<ApolloProvider client={null}/>);
+const ApolloConsumer = Apollo.Consumer;
+
+let app = (
+    <Apollo.Provider value={client}>
+        <App />
+    </Apollo.Provider>
+);
+
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export {ApolloConsumer};
